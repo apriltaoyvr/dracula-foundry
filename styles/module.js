f1945205e56dc4d29c816fd78a98ce9cb238eb5d
April Tao <95392008+CasanovaSekova@users.noreply.github.com>
2021-12-17 03:21:37 -0800

    Legibility Improvements
    Additional gradients, windows are more opaque, and other small changes. Thank you to @bakbak#2827 for noticing these issues and testing the UI for me.

---
+++ styles/module.js
@@ -0,0 +1,8 @@
+import App from ""
+import '@dracula/dracula-ui/styles/dracula-ui.css'
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
