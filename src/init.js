bfbad67c37db498cb46f3dd00e2447ae96942643
April Tao <apriltaoyvr@outlook.com>
2021-12-17 15:50:49 -0800

    Cleaning

--- src/init.js
+++ src/init.js
@@ -1,5 +1,6 @@
 Hooks.once('init', () => {
    const root = document.documentElement;
+   root.style.setProperty('--color-shadow-dark', hsl(230, 15%, 30%));
 
    // Example of setting the shadow root color; Noticeable around the sidebar.
    // root.style.setProperty('--color-shadow-dark', 'red');
