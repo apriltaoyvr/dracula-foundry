39bf05137c200dbdb14560502d81ff93e65ff4e1
April Tao <95392008+CasanovaSekova@users.noreply.github.com>
2021-12-16 21:43:57 -0800

    Update App.js

--- scripts/lib/App.js
+++ scripts/lib/App.js
@@ -1,8 +1,1 @@
 import '@dracula/dracula-ui/styles/dracula-ui.css'
-import { Paragraph } from '@dracula/dracula-ui'
-
-function App() {
-  return <Paragraph color="black">Hello Vampire</Paragraph>
-}
-
-export default App;
