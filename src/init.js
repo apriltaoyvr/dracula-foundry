5a7860e3f5584e5df3dc1a4f88a54925191716b0
April Tao <yourgothvalentine@outlook.com>
2021-12-18 00:28:41 -0800

    Adjustments to root values + rolltables

--- src/init.js
+++ src/init.js
@@ -1,7 +1,28 @@
 Hooks.once('init', () => {
-   const root = document.documentElement;
-   root.style.setProperty('--color-shadow-dark', hsl(230, 15%, 30%));
+	const root = document.documentElement;
+	root.style.setProperty('--color-underline-header', hsl(330, 100 % , 75 % ));
+	root.style.setProperty('--color-underline-active', hsl(10, 100 % , 75 % ));
+	root.style.setProperty('--color-underline-header', hsl(10, 100 % , 75 % ));
+	root.style.setProperty('--color-text-hyperlink', hsl(330, 100 % , 75 % ));
 
-   // Example of setting the shadow root color; Noticeable around the sidebar.
-   // root.style.setProperty('--color-shadow-dark', 'red');
+	root.style.setProperty('--color-shadow-primary', hsl(250, 100 % , 75 % ));
+	root.style.setProperty('--color-shadow-highlight', hsl(330, 100 % , 75 % ));
+	root.style.setProperty('--color-shadow-highlight-alt', hsl(330, 100 % , 75 % ));
+	root.style.setProperty('--color-border-highlight', hsl(330, 100 % , 75 % ));
+	root.style.setProperty('--color-border-highlight-alt', hsl(330, 100 % , 75 % ));
+
+	root.style.setProperty('--color-shadow-dark', hsl(230, 15 % , 30 % ));
+
+	root.style.setProperty('--color-text-light-highlight', hsl(60, 30 % , 96 % ));
+	root.style.setProperty('--color-text-light-heading', hsl(60, 30 % , 96 % ));
+	root.style.setProperty('--color-text-light-primary', hsl(60, 30 % , 100 % ));
+	root.style.setProperty('--color-text-dark-primary', hsl(230, 15 % , 15 % ));
+	root.style.setProperty('--color-text-dark-secondary', hsl(230, 15 % , 30 % ));
+	root.style.setProperty('--color-text-dark-header', hsl(230, 15 % , 15 % ));
+	root.style.setProperty('--color-text-dark-inactive', hsl(230, 15 % , 50 % ));
+
+	// root.style.setProperty('', );
+
+	// Example of setting the shadow root color; Noticeable around the sidebar.
+	// root.style.setProperty('--color-shadow-dark', 'red');
 });
