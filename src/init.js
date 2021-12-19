b0d0339e8746e272257a9acd69b45c97aa20a165
April Tao <yourgothvalentine@outlook.com>
2021-12-19 00:06:44 -0800

    HEX to RGB

--- src/init.js
+++ src/init.js
@@ -1,25 +1,57 @@
 Hooks.once('init', () => {
 	const root = document.documentElement;
-	root.style.setProperty('--color-underline-header', hsl(330, 100 % , 75 % ));
-	root.style.setProperty('--color-underline-active', hsl(10, 100 % , 75 % ));
-	root.style.setProperty('--color-underline-header', hsl(10, 100 % , 75 % ));
-	root.style.setProperty('--color-text-hyperlink', hsl(330, 100 % , 75 % ));
+	root.style.setProperty('--color-underline-header', rgb(149, 128, 255));
 
-	root.style.setProperty('--color-shadow-primary', hsl(250, 100 % , 75 % ));
-	root.style.setProperty('--color-shadow-highlight', hsl(330, 100 % , 75 % ));
-	root.style.setProperty('--color-shadow-highlight-alt', hsl(330, 100 % , 75 % ));
-	root.style.setProperty('--color-border-highlight', hsl(330, 100 % , 75 % ));
-	root.style.setProperty('--color-border-highlight-alt', hsl(330, 100 % , 75 % ));
 
-	root.style.setProperty('--color-shadow-dark', hsl(230, 15 % , 30 % ));
 
-	root.style.setProperty('--color-text-light-highlight', hsl(60, 30 % , 96 % ));
-	root.style.setProperty('--color-text-light-heading', hsl(60, 30 % , 96 % ));
-	root.style.setProperty('--color-text-light-primary', hsl(60, 30 % , 100 % ));
-	root.style.setProperty('--color-text-dark-primary', hsl(230, 15 % , 15 % ));
-	root.style.setProperty('--color-text-dark-secondary', hsl(230, 15 % , 70 % ));
-	root.style.setProperty('--color-text-dark-header', hsl(230, 15 % , 15 % ));
-	root.style.setProperty('--color-text-dark-inactive', hsl(230, 15 % , 50 % ));
+
+
+	root.style.setProperty('--color-underline-active', rgb(149, 128, 255));
+	root.style.setProperty('--color-text-hyperlink', rgb(255, 128, 191));
+
+	root.style.setProperty('--color-shadow-primary', rgb(149, 128, 255));
+
+
+
+
+
+	root.style.setProperty('--color-shadow-highlight', rgb(255, 128, 191));
+	root.style.setProperty('--color-shadow-highlight-alt', rgb(149, 128, 255));
+
+
+
+
+
+	root.style.setProperty('--color-border-highlight', rgb(255, 128, 191));
+	root.style.setProperty('--color-border-highlight-alt', rgb(149, 128, 255));
+
+
+
+
+
+
+
+
+
+	root.style.setProperty('--color-shadow-dark', rgb(65, 69, 88));
+
+	root.style.setProperty('--color-text-light-highlight', rgb(248, 248, 242));
+	root.style.setProperty('--color-text-light-heading', rgb(248, 248, 242));
+	root.style.setProperty('--color-text-light-primary', rgb(248, 248, 242));
+	root.style.setProperty('--color-text-dark-primary', rgb(33, 34, 44));
+
+
+
+
+
+	root.style.setProperty('--color-text-dark-secondary', rgb(108, 115, 147));
+	root.style.setProperty('--color-text-dark-header', rgb(33, 34, 44));
+
+
+
+
+
+	root.style.setProperty('--color-text-dark-inactive', rgb(108, 115, 147));
 
 	// root.style.setProperty('', );
 
