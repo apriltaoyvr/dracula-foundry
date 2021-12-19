2ead8c0dfd9a0057ade71735a02a37392227d497
April Tao <95392008+CasanovaSekova@users.noreply.github.com>
2021-12-18 23:57:04 -0800

    HSL to HEX
    Because my editor hates the spaces in HSL

--- src/init.js
+++ src/init.js
@@ -1,28 +1,21 @@
 Hooks.once('init', () => {
 	const root = document.documentElement;
-	root.style.setProperty('--color-underline-header', hsl(330, 100 % , 75 % ));
-	root.style.setProperty('--color-underline-active', hsl(10, 100 % , 75 % ));
-	root.style.setProperty('--color-underline-header', hsl(10, 100 % , 75 % ));
-	root.style.setProperty('--color-text-hyperlink', hsl(330, 100 % , 75 % ));
-
-	root.style.setProperty('--color-shadow-primary', hsl(250, 100 % , 75 % ));
-	root.style.setProperty('--color-shadow-highlight', hsl(330, 100 % , 75 % ));
-	root.style.setProperty('--color-shadow-highlight-alt', hsl(330, 100 % , 75 % ));
-	root.style.setProperty('--color-border-highlight', hsl(330, 100 % , 75 % ));
-	root.style.setProperty('--color-border-highlight-alt', hsl(330, 100 % , 75 % ));
-
-	root.style.setProperty('--color-shadow-dark', hsl(230, 15 % , 30 % ));
-
-	root.style.setProperty('--color-text-light-highlight', hsl(60, 30 % , 96 % ));
-	root.style.setProperty('--color-text-light-heading', hsl(60, 30 % , 96 % ));
-	root.style.setProperty('--color-text-light-primary', hsl(60, 30 % , 100 % ));
-	root.style.setProperty('--color-text-dark-primary', hsl(230, 15 % , 15 % ));
-	root.style.setProperty('--color-text-dark-secondary', hsl(230, 15 % , 70 % ));
-	root.style.setProperty('--color-text-dark-header', hsl(230, 15 % , 15 % ));
-	root.style.setProperty('--color-text-dark-inactive', hsl(230, 15 % , 50 % ));
-
+	
 	// root.style.setProperty('', );
-
-	// Example of setting the shadow root color; Noticeable around the sidebar.
-	// root.style.setProperty('--color-shadow-dark', 'red');
+	root.style.setProperty('--color-underline-header', #9580ff);
+	root.style.setProperty('--color-underline-active', #9580ff);
+	root.style.setProperty('--color-text-hyperlink', #ff80bf);
+	root.style.setProperty('--color-shadow-primary', #9580ff);
+	root.style.setProperty('--color-shadow-highlight', #ff80bf);
+	root.style.setProperty('--color-shadow-highlight-alt', #9580ff);
+	root.style.setProperty('--color-border-highlight', #ff80bf);
+	root.style.setProperty('--color-border-highlight-alt', #9580ff);
+	root.style.setProperty('--color-shadow-dark', #414558);
+	root.style.setProperty('--color-text-light-highlight', #f8f8f2);
+	root.style.setProperty('--color-text-light-heading', #f8f8f2);
+	root.style.setProperty('--color-text-light-primary', #f8f8f2);
+	root.style.setProperty('--color-text-dark-primary', #21222c);
+	root.style.setProperty('--color-text-dark-secondary', #6c7393);
+	root.style.setProperty('--color-text-dark-header', #21222c);
+	root.style.setProperty('--color-text-dark-inactive', #6c7393);
 });
