be19a28e796e97dd1278dae0b3022444524c59c5
April Tao <yourgothvalentine@outlook.com>
2021-12-19 05:44:45 -0800

    5e Character Sheets + Modules
    Added additional 5e character sheet integration + dae, dice tray, filepicker+, and lootsheetnpc compatibility

--- src/init.js
+++ src/init.js
@@ -1,19 +1,19 @@
-Hooks.once('init', () => {
+Hooks.once("init", () => {
 	const root = document.documentElement;
-	root.style.setProperty('--color-underline-header', rgb(149, 128, 255));
-	root.style.setProperty('--color-underline-active', rgb(149, 128, 255));
-	root.style.setProperty('--color-text-hyperlink', rgb(255, 128, 191));
-	root.style.setProperty('--color-shadow-primary', rgb(149, 128, 255));
-	root.style.setProperty('--color-shadow-highlight', rgb(255, 128, 191));
-	root.style.setProperty('--color-shadow-highlight-alt', rgb(149, 128, 255));
-	root.style.setProperty('--color-border-highlight', rgb(255, 128, 191));
-	root.style.setProperty('--color-border-highlight-alt', rgb(149, 128, 255));
-	root.style.setProperty('--color-shadow-dark', rgb(65, 69, 88));
-	root.style.setProperty('--color-text-light-highlight', rgb(248, 248, 242));
-	root.style.setProperty('--color-text-light-heading', rgb(248, 248, 242));
-	root.style.setProperty('--color-text-light-primary', rgb(248, 248, 242));
-	root.style.setProperty('--color-text-dark-primary', rgb(33, 34, 44));
-	root.style.setProperty('--color-text-dark-secondary', rgb(108, 115, 147));
-	root.style.setProperty('--color-text-dark-header', rgb(33, 34, 44));
-	root.style.setProperty('--color-text-dark-inactive', rgb(108, 115, 147));
+	root.style.setProperty("--color-underline-header", rgb(149, 128, 255));
+	root.style.setProperty("--color-underline-active", rgb(149, 128, 255));
+	root.style.setProperty("--color-text-hyperlink", rgb(255, 128, 191));
+	root.style.setProperty("--color-shadow-primary", rgb(149, 128, 255));
+	root.style.setProperty("--color-shadow-highlight", rgb(255, 128, 191));
+	root.style.setProperty("--color-shadow-highlight-alt", rgb(149, 128, 255));
+	root.style.setProperty("--color-border-highlight", rgb(255, 128, 191));
+	root.style.setProperty("--color-border-highlight-alt", rgb(149, 128, 255));
+	root.style.setProperty("--color-shadow-dark", rgb(65, 69, 88));
+	root.style.setProperty("--color-text-light-highlight", rgb(248, 248, 242));
+	root.style.setProperty("--color-text-light-heading", rgb(248, 248, 242));
+	root.style.setProperty("--color-text-light-primary", rgb(248, 248, 242));
+	root.style.setProperty("--color-text-dark-primary", rgb(33, 34, 44));
+	root.style.setProperty("--color-text-dark-secondary", rgb(108, 115, 147));
+	root.style.setProperty("--color-text-dark-header", rgb(33, 34, 44));
+	root.style.setProperty("--color-text-dark-inactive", rgb(108, 115, 147));
 });
