ab0825695817a344f17756ebd97ea619f4314c30
CasanovaSekova <apriltaoyvr@outlook.com>
2021-12-25 05:31:42 -0800

    Fixing combat carousel + others

--- src/init.js
+++ src/init.js
@@ -63,11 +63,11 @@ function modifyCSSVariables() {
 /**
  * Override box-shadow root and colour box-shadow with Minimal UI's box shadow setting
  * Courtesy of Jeansen himself
-
-function minimalBoxShadow {
-   if (game.modules.get('minimal-ui')?.active) {
-      rootStyle.setProperty('--any1-color-property', game.settings.get('minimal-ui', 'shadowColor'));
-      rootStyle.setProperty('--any2-color-property', game.settings.get('minimal-ui', 'borderColor'));
-   }
+ * NOTE: Currently will override accent colour and most gradients. I'll think of a way to link the two proper in a bit.
+   function minimalBoxShadow {
+      if (game.modules.get('minimal-ui')?.active) {
+         rootStyle.setProperty('--any1-color-property', game.settings.get('minimal-ui', 'shadowColor'));
+         rootStyle.setProperty('--any2-color-property', game.settings.get('minimal-ui', 'borderColor'));
+      }
 }
  */
