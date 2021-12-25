110b4ff3a0709510b2d6266b0d7cc32d6580bdc1
CasanovaSekova <apriltaoyvr@outlook.com>
2021-12-25 08:45:59 -0800

    It was a fun experiment but definitely not worth it

--- src/ModuleSettings.js
+++ src/ModuleSettings.js
@@ -41,16 +41,5 @@ export default class ModuleSettings {
             document.documentElement.style.setProperty('--filter-dracula-backdrop', value ? 'blur(12px)' : 'none');
          }
       });
-      game.settings.register(constants.moduleName, settings.iconFont, {
-         name: 'Change Font Awesome icons to Line Awesome icons',
-         hint: 'Line Awesome icons are smaller and much less legible than Font Awesome. For the people out there who want extra, extra small icons.',
-         scope: scope.client,
-         config: true,
-         default: false,
-         type: Boolean,
-         onChange: (value) => {
-            $(`link[href="fonts/fontawesome/css/all.min.css"]`)[0].href = "https://maxst.icons8.com/vue-static/landings/line-awesome/font-awesome-line-awesome/css/all.min.css";
-         }
-      });
    }
 }
